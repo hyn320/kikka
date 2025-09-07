6e1429a1cd775acbabc5d7f9363f9fe906c69226
hyn320 <al25041@sic.shibaura-it.ac.jp>
2025-09-07 11:01:57 +0900

    動画再生後/画面タップ後にフェードアウトインでホーム画面に遷移

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Profile from "./pages/Profile";
 import Movie from "./components/Movie";
-// import SpecialScreen from "./pages/SpecialScreen";
+import styles from "./App.module.css";  // ← module.cssに変更
 
 export default function App() {
   const [showVideo, setShowVideo] = useState(true);
-  // const [showSpecial, setShowSpecial] = useState(true);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  const handleMovieFinish = () => {
+    setShowVideo(false);
+    setTimeout(() => setFadeIn(true), 50); 
+    // 遷移時に一瞬遅らせてフェードイン開始
+  };
 
   return (
     <Router>
       {showVideo ? (
-        <Movie onFinish={() => setShowVideo(false)} />
+        <Movie onFinish={handleMovieFinish} />
       ) : (
-        <>
-
-      {/* {showSpecial ? (
-        <SpecialScreen onFinish={() => setShowSpecial(false)} />
-      ) : (
-        <> */}
-
+        <div className={`${styles.pageWrapper} ${fadeIn ? styles.show : ""}`}>
+          <div className={`${styles.overlay} ${fadeIn ? styles.hide : ""}`} />
           <div className="flex justify-around bg-gray-100 p-4">
             <Link to="/">ホーム</Link>
             <Link to="/gallery">ギャラリー</Link>
@@ -29,15 +30,12 @@ export default function App() {
           </div>
 
           <Routes>
-           {/* {showVideo && <Movie />} */}
             <Route path="/" element={<Home />} />
             <Route path="/gallery/*" element={<Gallery />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
-
-        </>
-      )} 
+        </div>
+      )}
     </Router>
   );
 }
-
